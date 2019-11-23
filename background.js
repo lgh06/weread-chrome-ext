178cfaf5d85834009038934253c0871f80b8bb52
lgh06 <hnnk@qq.com>
2019-11-23 19:12:20 +0800

    following https://developer.chrome.com/extensions/activeTab to do the full screen feature

--- background.js
+++ background.js
@@ -4,8 +4,17 @@
 
 'use strict';
 
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.sync.set({color: '#3aa757'}, function() {
-//     console.log("The color is green.");
-//   });
-// });
+let func = function() {
+  document.documentElement.requestFullscreen()
+}
+
+let funcStr = '(' + func.toString() + ')()'
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: funcStr
+  });
+});
