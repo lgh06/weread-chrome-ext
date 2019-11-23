7e198769253e561d5d8eb8c8fcd7f6b689021378
lgh06 <hnnk@qq.com>
2019-11-23 20:08:36 +0800

    update css and icon

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 let func = function() {
@@ -10,11 +6,42 @@ let func = function() {
 
 let funcStr = '(' + func.toString() + ')()'
 
+let cssStr = `
+.app_content[class] {
+  max-width: 1600px;
+}
+.readerContent .app_content[class] {
+  background-color: transparent;
+}
+.readerTopBar[class] {
+  position: static;
+}
+.readerChapterContent[class] {
+  zoom: 1.25;
+  margin: auto 0;
+}
+.readerChapterContent[class] span[class] {
+  zoom: 1
+}
+.readerControls[class] {
+  right: 0;
+  margin-left: 0;
+  left: initial;
+}
+body::-webkit-scrollbar {
+  width: 0;
+}
+`
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: funcStr
-  });
+  if (tab.url.match('weread.qq.com/web/reader/')) {
+    chrome.tabs.executeScript({
+      code: funcStr
+    });
+    chrome.tabs.insertCSS({
+      code: cssStr
+    });
+  }
 });
